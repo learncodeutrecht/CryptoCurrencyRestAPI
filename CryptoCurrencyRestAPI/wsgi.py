11d11f8d698341ae66c5b0d78afbf26f7538fa90
RikSchoonbeek <rik.schoonbeek@gmail.com>
2018-04-11 23:26:07 +0200

    Added allowed_hosts to wsgi.py

--- CryptoCurrencyRestAPI/wsgi.py
+++ CryptoCurrencyRestAPI/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CryptoCurrencyRestAPI.settings")
 
 application = get_wsgi_application()
+
+ALLOWED_HOSTS = ['128.199.35.153']
